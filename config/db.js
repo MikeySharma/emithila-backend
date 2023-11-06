280f31c64ae30540f7a7c66a7ea014e33d5e6646
MikeySharma <mikeysharma99@gmail.com>
2023-11-06 21:41:08 +0545

    render upload changes

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectToMongo = () =>{
     try{
-        mongoose.connect(process.env.MONGODB_URL);
+        mongoose.connect(process.env.MONGODB_URL,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
     }catch(error){
         console.log(error);
     }

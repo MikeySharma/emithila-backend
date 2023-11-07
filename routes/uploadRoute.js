85716a247b6150078b3e8e6e5e3a4ae6034decc0
MikeySharma <mikeysharma99@gmail.com>
2023-11-07 15:36:55 +0545

    upload images to the server

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -10,9 +10,9 @@ const {
 const {  uploadPhoto } = require('../middleware/uploadImages');
 
 
-router.put('/', authMiddleware, isAdmin,uploadPhoto, uploadImages );
+router.put('/', authMiddleware, isAdmin,uploadPhoto.array("image", 10), uploadImages );
 router.delete('/delete-img/:id',authMiddleware, isAdmin, deleteImages);
-router.put('/blog', authMiddleware, isAdmin, uploadPhoto, uploadBlogImages);
+router.put('/blog', authMiddleware, isAdmin, uploadPhoto.array("image", 10), uploadBlogImages);
 
 
 module.exports = router;

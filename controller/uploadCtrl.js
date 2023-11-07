208a704cc71662b6f5e5a795be020102a3b49d4b
MikeySharma <mikeysharma99@gmail.com>
2023-11-07 18:03:03 +0545

    upload to the server

--- controller/uploadCtrl.js
+++ controller/uploadCtrl.js
@@ -5,7 +5,7 @@ const asyncHandler = require('express-async-handler');
 const uploadImages = asyncHandler (async (req, res)=>{
     try{
         const urls = [];
-        const files = req.files;
+        const files = Array.from(req.files);
         for(const file of files){
             const {buffer} = file;
             const newPath = await cloudinaryImgUploading(buffer);

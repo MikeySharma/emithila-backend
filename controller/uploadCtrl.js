a960d1c3e7cde5ad1a1c8af59f62d3a5418c155b
MikeySharma <mikeysharma99@gmail.com>
2023-11-09 16:29:12 +0545

    some bug fixes for emithila-web app

--- controller/uploadCtrl.js
+++ controller/uploadCtrl.js
@@ -1,13 +1,13 @@
-const {cloudinaryImgUploading, cloudinaryDeleteImg} = require('../utils/cloudinary');
+const { cloudinaryImgUploading, cloudinaryDeleteImg } = require('../utils/cloudinary');
 const asyncHandler = require('express-async-handler');
 
 //upload product Images
-const uploadImages = asyncHandler (async (req, res)=>{
-    try{
+const uploadImages = asyncHandler(async (req, res) => {
+    try {
         const urls = [];
         const files = req.files;
-        for(const file of files){
-            const {buffer} = file;
+        for (const file of files) {
+            const { buffer } = file;
             const newPath = await cloudinaryImgUploading(buffer);
             urls.push(newPath);
 
@@ -16,46 +16,46 @@ const uploadImages = asyncHandler (async (req, res)=>{
             return file;
         })
         res.json(images);
-    }catch(error){
+    } catch (error) {
         throw new Error(error);
     }
 })
 //Delete product Images
-const deleteImages = asyncHandler (async (req, res)=>{
-    const {id} = req.params;
-    try{
+const deleteImages = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
 
-        const deleter =  cloudinaryDeleteImg(id, "images");
-        res.json({id : id, message: "Deleted"})
-       
-    }catch(error){
+        const deleter = cloudinaryDeleteImg(id, "images");
+        res.json({ id: id, message: "Deleted" })
+
+    } catch (error) {
         throw new Error(error);
     }
 })
 
 //upload blog Images
-const uploadBlogImages = asyncHandler (async (req, res)=>{
-   
-    try{
-        
+const uploadBlogImages = asyncHandler(async (req, res) => {
+
+    try {
+
         const urls = [];
         const files = req.files;
-        for(const file of files){
-            const {buffer} = file;
+        for (const file of files) {
+            const { buffer } = file;
             const newPath = await cloudinaryImgUploading(buffer);
             urls.push(newPath);
-            
+
         }
         const images = urls.map((file) => {
             return file;
         })
         res.json(images);
-    }catch(error){
+    } catch (error) {
         throw new Error(error);
     }
 })
 
-module.exports ={
+module.exports = {
     uploadImages,
     uploadBlogImages,
     deleteImages,
